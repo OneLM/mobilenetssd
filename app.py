73e5878a5d49646df12e19a8008332cf02fa069c
Ploy pakamon <91713575+OneLM@users.noreply.github.com>
2021-11-23 17:28:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        if (msg == "สวัสดี") :
+        if (msg == "หวัดดี") :
             replyObj = TextSendMessage(text="ดีครับ")
             line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "กินข้าวยัง") :
